9ad5539a3a8866fdd77a8afcbdfcc84d83c1ed71
ChefnCoder <tanmay2020anand@gmail.com>
2024-01-28 16:01:41 +0530

    updated button click

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 (function() {
-
     let screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
     let clear = document.querySelector('.btn-clear');
     let equal = document.querySelector('.btn-equal');
+    
 
     buttons.forEach(function(button) {
         button.addEventListener('click', function(e) {
+            var computedStyle = window.getComputedStyle(button);
+            var backgroundColor = computedStyle.backgroundImage;
+
+            button.style.backgroundImage = "linear-gradient(rgb(0, 0, 0), rgb(0,0,0)";
+            setTimeout(function(){
+                button.style.backgroundImage = backgroundColor;
+            },100)
+            let value = e.target.dataset.num;
+            screen.value += value;
             
-                let value = e.target.dataset.num;
-                screen.value += value;
-            })
+
+        });
     });
-    
+     
 
     equal.addEventListener('click', function(e){
         if(screen.value == ""){
             clearScreen();
-        }else{
+        } else {
             let answer = eval(screen.value);
-            clearScreen;
+            clearScreen();
             screen.value = answer;
         }
-    })
+    });
 
-    clear.addEventListener('click', clearScreen)
+    clear.addEventListener('click', clearScreen);
 
     function clearScreen(){
         screen.value = "";
