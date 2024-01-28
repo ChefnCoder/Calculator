0c2eb5bc8db02688bc12e8a7e78395107ddb3f25
ChefnCoder <tanmay2020anand@gmail.com>
2024-01-28 20:47:00 +0530

    clickeffect function made

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
 (function() {
     let screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
-    
+    let clearbtn = document.querySelector('.btn-clear');
+    let equalbtn = document.querySelector('.btn-equal');
 
     buttons.forEach(function(button) {
         button.addEventListener('click', function(e) {
-            var computedStyle = window.getComputedStyle(button);
-            var backgroundColor = computedStyle.backgroundImage;
+            
+            if (button.classList.contains('btn-yellow')){
+                clickeffect(button,"linear-gradient(rgb(214, 110, 7), rgb(214, 110, 7))")
+            }
+            if (button.classList.contains('btn-grey')){
+                clickeffect(button,"linear-gradient(rgb(0, 0, 0), rgb(0,0,0))")
+            }
 
-            button.style.backgroundImage = "linear-gradient(rgb(0, 0, 0), rgb(0,0,0)";
-            setTimeout(function(){
-                button.style.backgroundImage = backgroundColor;
-            },100)
             let value = e.target.dataset.num;
             screen.value += value;
-            
-
         });
     });
      
 
-    equal.addEventListener('click', function(e){
+    equalbtn.addEventListener('click', function(e){
+        
+        clickeffect(equalbtn,"linear-gradient(rgb(2, 75, 2),rgb(2, 75, 2))")
+
         if(screen.value == ""){
             clearScreen();
         } else {
             let answer = eval(screen.value);
             clearScreen();
-            screen.value = answer;
+            screen.value = answer;  
         }
     });
 
-    clear.addEventListener('click', clearScreen);
+    clearbtn.addEventListener('click', function(){
+
+        clickeffect(clearbtn,"linear-gradient(red,red)")
+        clearScreen();
+
+    });
 
     function clearScreen(){
         screen.value = "";
+    }
+    function clickeffect(buttoname,color){
+        var computedStyle = window.getComputedStyle(buttoname);
+        var backgroundColor = computedStyle.backgroundImage;
+        buttoname.style.backgroundImage = color;
+        setTimeout(function(){
+            buttoname.style.backgroundImage = backgroundColor;
+        },100)
     }
 })();
